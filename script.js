ec2daa1994fb35a84b3953980e4c056da853734a
nbeckles <nbeckles@gmail.com>
2021-10-14 19:38:34 -0400

    change root font and bg colour

--- script.js
+++ script.js
@@ -3,32 +3,38 @@ let gridWidth = 75;
 let container = document.querySelector(".container");
 
 
-function createGrid(width){
-    for (i = 0; i < (width*width); i++) {
+function createGrid(width) {
+    for (i = 0; i < (width * width); i++) {
         let divnode = document.createElement("div");
         divnode.className = "grid-element";
-        divnode.classList.add("item"+(i+1));
+        divnode.classList.add("item" + (i + 1));
         container.appendChild(divnode);
     }
 }
 
-function clearGrid(){
+function clearGrid() {
 
 }
 
 createGrid(gridWidth);
-
+let pointerType = "pencil";
 let gridSquare = document.querySelectorAll(".grid-element");
 gridSquare.forEach((div) => {
-    div.addEventListener("mouseenter", function(e){
+    div.addEventListener("mouseenter", function (e) {
         //this.classList.add("grid-item-hover");
-        let gridBGColor = getComputedStyle(document.querySelector(":root")).getPropertyValue("--line-color");
-        this.style.setProperty("background-color",gridBGColor);
+        if (pointerType === "pencil") {
+            let gridBGColor = getComputedStyle(document.querySelector(":root")).getPropertyValue("--line-color");
+            this.style.setProperty("background-color", gridBGColor);
+        }
+    });
+    div.addEventListener("click", function(e){
+        if (pointerType==="eraser"){
+            this.style.setProperty("background-color", "white");
+        }
     });
 });
 
 let lineColor = document.querySelector("#line-colour");
-lineColor.addEventListener('change',()=>{
-    console.log(lineColor);
-    document.querySelector(":root").style.setProperty("--line-color",lineColor.value);
+lineColor.addEventListener('change', () => {
+    document.querySelector(":root").style.setProperty("--line-color", lineColor.value);
 });
