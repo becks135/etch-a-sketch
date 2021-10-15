d5288cb6a1f4fcfa474c9be22f66334a76e7e9df
nbeckles <nbeckles@gmail.com>
2021-10-14 22:00:45 -0400

    add clear functionality

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function clearGrid() {
 createGrid(gridWidth);
 let pointerType = "pencil";
 let gridSquare = document.querySelectorAll(".grid-element");
+
 gridSquare.forEach((div) => {
     div.addEventListener("mouseenter", function (e) {
         //this.classList.add("grid-item-hover");
@@ -27,8 +28,8 @@ gridSquare.forEach((div) => {
             this.style.setProperty("background-color", gridBGColor);
         }
     });
-    div.addEventListener("click", function(e){
-        if (pointerType==="eraser"){
+    div.addEventListener("click", function (e) {
+        if (pointerType === "eraser") {
             this.style.setProperty("background-color", "white");
         }
     });
@@ -36,17 +37,23 @@ gridSquare.forEach((div) => {
 
 let lineColor = document.querySelector("#line-colour");
 lineColor.addEventListener('change', () => {
-   
+
     document.querySelector(":root").style.setProperty("--line-color", lineColor.value);
 });
 
 let eraser = document.querySelector(".eraser");
-eraser.addEventListener('click', function(e){
-    pointerType="eraser";
+eraser.addEventListener('click', function (e) {
+    pointerType = "eraser";
     document.documentElement.style.cursor = "url('clipart42077192.png'), default";
 });
 
-document.querySelector(".color-selection").addEventListener('click',function(e){
+document.querySelector(".color-selection").addEventListener('click', function (e) {
     document.documentElement.style.cursor = "default";
-    pointerType="pencil";
+    pointerType = "pencil";
+});
+
+document.querySelector(".clear").addEventListener('click', function (e) {
+    gridSquare.forEach((div)=>{
+        div.style.setProperty("background-color", "white");
+    });
 });
